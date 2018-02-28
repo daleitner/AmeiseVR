305564ad27efcd427e7ad0552dc2b9b8397c70f8
Daniel Leitner <dmcleite@hotmail.com>
2018-02-28 14:51:22 +0100

    implemented ClientConnection

---
+++ AMEISE_VR/Assets/Scripts/ClientConnection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+public class ClientConnection
+{
+	private readonly Socket _socket;
+	public ClientConnection()
+	{
+		_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+	}
+
+	public void Connect(string ipAdress, int port)
+	{
+		var adress = IPAddress.Parse(ipAdress);
+		var endPoint = new IPEndPoint(adress, port);
+		_socket.Connect(endPoint);
+	}
+
+	public void CloseConnection()
+	{
+		if(_socket.Connected)
+			_socket.Close();
+	}
+}
