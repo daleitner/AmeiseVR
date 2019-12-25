950a70509a9f22096ab0889356804a6e1bec3b34
Daniel Leitner <dmcleite@hotmail.com>
2019-12-25 10:44:02 +0100

    changed colors of tasks

--- AMEISE_VR/Assets/Scripts/Task.cs
+++ AMEISE_VR/Assets/Scripts/Task.cs
@@ -7,8 +7,11 @@ namespace Assets.Scripts
 {
 	public class Task : GameObjectModelBase
 	{
-		private static readonly Color DefaultColor = new Color(1.0f, 0.16f, 0.13f);
-		private static readonly Color SelectedColor = new Color(0.16f, 1.0f, 0.22f);
+		private static readonly Color DefaultColor = new Color(0.25f, 0.5f, 1.0f);
+		private static readonly Color SelectedColor = new Color(0.25f, 0.8f, 1.0f);
+		private static readonly Color ErrorColor = new Color(1.0f, 0.16f, 0.13f);
+		private static readonly Color SuccessColor = new Color(0.16f, 1.0f, 0.22f);
+		private static readonly Color IncompleteColor = new Color(1.0f, 0.875f, 0.25f);
 		private readonly TextMeshPro _text;
 		private readonly Material _material;
 		private int _currentParameterIndex = -1;
@@ -33,6 +36,9 @@ namespace Assets.Scripts
 		
 		public Command Command { get; }
 
+		public bool IsSent { get; private set; }
+		public bool IsIncomplete { get; private set; }
+
 		public bool IsSelected { get; private set; }
 
 		public void ChangeParameter()
@@ -48,6 +54,8 @@ namespace Assets.Scripts
 
 		public void SetSelection(bool selected)
 		{
+			if (IsSent)
+				return;
 			IsSelected = selected;
 			_material.color = selected ? SelectedColor : DefaultColor;
 		}
@@ -58,6 +66,30 @@ namespace Assets.Scripts
 			task._currentParameterIndex = _currentParameterIndex;
 			task.SetText();
 			return task;
+		}
+
+		public void TaskSentSuccessful()
+		{
+			IsSent = true;
+			_material.color = SuccessColor;
+		}
+
+		public void TaskSentError()
+		{
+			IsSent = true;
+			_material.color = ErrorColor;
+		}
+
+		public void SetIncomplete()
+		{
+			IsIncomplete = true;
+			_material.color = IncompleteColor;
+		}
+
+		public void SetComplete()
+		{
+			IsIncomplete = false;
+			_material.color = DefaultColor;
 		}
 
 		private void SetText()
