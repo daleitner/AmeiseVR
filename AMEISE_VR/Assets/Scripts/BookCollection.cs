9fa053db3a5885087668feee4d364f91e5f33aaf
Daniel Leitner <dmcleite@hotmail.com>
2020-03-24 06:39:07 +0100

    increased distance between employees books

--- AMEISE_VR/Assets/Scripts/BookCollection.cs
+++ AMEISE_VR/Assets/Scripts/BookCollection.cs
@@ -13,7 +13,7 @@ namespace Assets.Scripts
 		private readonly Vector3 DefaultPosition = new Vector3(0.01f, 0.35f, -0.25f);
 		private readonly Vector3 DefaultScale = new Vector3(0.85f, 1.0f, 1.7f);
 		private readonly Quaternion DefaultRotation = Quaternion.Euler(90.0f, 180.0f, 90.0f);
-		private const float x_offset = 0.04f;
+		private const float x_offset = 0.2f;
 		public BookCollection(GameObject shelf)
 		{
 			_shelf = shelf;
