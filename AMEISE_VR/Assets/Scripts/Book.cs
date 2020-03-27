96addb70e0d2ee6ac9631ad918ad2f5e8980811e
Daniel Leitner <dmcleite@hotmail.com>
2020-03-27 15:49:34 +0100

    fixed build errors. removed username and password for testing

--- AMEISE_VR/Assets/Scripts/Book.cs
+++ AMEISE_VR/Assets/Scripts/Book.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq;
-using Boo.Lang;
+﻿using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
--- AMEISE_VR/Assets/Scripts/PlayerBoardCollection.cs
+++ AMEISE_VR/Assets/Scripts/PlayerBoardCollection.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
-using Unity.UNetWeaver;
 using UnityEngine;
 
 namespace Assets.Scripts
